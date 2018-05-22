d7da8694258c09e090b16eef863cf91dc3b903ed
dengpeng <dp@tiejin.cn>
2018-05-22 11:14:06 +0800

    Fix 奖励金规则，提现规则

---
+++ pages/about/bonusrole.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="about">
+    <div class="about">
+      <div class="part">
+        <h3>1、什么是奖励金？</h3>
+        <p>为鼓励用户分享，贴近平台对将对用户成功拉新的行为给予的现金奖励；</p>
+        <h3>2、如何获取奖励金？</h3>
+        <p>新用户通过您分享的链接下载贴近客户端并注册账号后，奖励金将自动发放到您的账户中；</p>
+        <h3>3、怎么查看奖励金账户？</h3>
+        <p>登录贴近客户端-我的-我的钱包-奖励金；</p>
+        <h3>4、为什么我有一笔奖励金，但是无法使用？</h3>
+        <p>您收到的奖励金属于冻结状态（未解冻），不能立即提现。根据您邀请的用户登陆贴近APP的天数，冻结余额会逐步解冻。具体规则：对应新用户每登陆一天，账户解冻0.5元；</p>
+        <h3>5、奖励金是否可以提现？</h3>
+        <p>已解冻部分奖励金将自动放置于我的钱包-可用余额中，达到钱包提现规定余额时即可操作提现。</p>
+        <nuxt-link to="/about/withdrawrole">钱包提现规则详见：贴近客户端-我的-我的钱包-提现。</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+#about {
+  padding: 0.22rem;
+  padding-top: 0.88rem;
+  line-height: 1.6;
+}
+
+.part {
+  margin-bottom: 0.2rem;
+  text-align: justify;
+}
+.part a {
+  color: #409eff;
+}
+h3,
+p {
+  margin-bottom: 0.2rem;
+}
+h3 {
+  color: #444;
+}
+</style>
---
+++ pages/about/withdrawrole.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="about">
+    <div class="about">
+      <div class="part">
+        <h3>1、支付宝绑定</h3>
+        <p>钱包提现目前仅支持支付宝提现，提现前您需要绑定一个支付宝账户。</p>
+        <p><a href="javascript:;">具体操作：我的-我的钱包-绑定支付宝-确认绑定；</a></p>
+        <h3>2、提现门槛</h3>
+        <p>每次提现金额必须大于或等于30元，账户余额低于30元时无法提现；</p>
+        <h3>3、当日提现上限</h3>
+        <p>每天提现总金额不得超过800元</p>
+        <h3>4、身份证验证</h3>
+        <p>提现累计总金额超过100元时，必须绑定身份证；</p>
+        <p><a href="javascript:;">具体操作：我的-我的钱包-身份证验证-输入您的姓名与身份证号-点击上传-等待后台审核通过，完成验证。</a></p>
+        <br />
+        <h3>温馨提示：</h3>
+        <p>1、账户余额低于30元时，您可通过向贴近号投稿赚取稿费或者拉新用户来获得更多的账户余额；</p>
+        <p>2、如您的提现额度已达当日上限，请明日再试。</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+#about {
+  padding: 0.22rem;
+  padding-top: 0.88rem;
+  line-height: 1.6;
+}
+
+.part {
+  margin-bottom: 0.2rem;
+  text-align: justify;
+}
+
+h3,
+p {
+  margin-bottom: 0.2rem;
+}
+h3 {
+  color: #444;
+}
+</style>
