106c915e6b97842a9e006cc4d1ca5213282ad9fb
dengpeng <dp@tiejin.cn>
2018-07-05 15:11:50 +0800

    Fix redirect中转

---
+++ layouts/redirect.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="redirect">
+    <div class="not-exist flex flex-v flex-align-center">
+      正在跳转中...
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "redirect",
+  beforeMount() {
+    // console.log(this.$route.query.redirectUrl.indexOf("?") > -1);
+    // console.log(this.$route.query.code);
+    if (this.$route.query.redirectUrl.indexOf("?") > -1) {
+      location.href = `${this.$route.query.redirectUrl}&code=${
+        this.$route.query.code
+      }`;
+    } else {
+      location.href = `${this.$route.query.redirectUrl}?code=${
+        this.$route.query.code
+      }`;
+    }
+  }
+};
+</script>
+<style>
+.not-exist {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+
--- pages/redirect/index.vue
+++ pages/redirect/index.vue
@@ -1,28 +1,5 @@
-<template>
-  <div class="redirect">
-    <div class="not-exist flex flex-v flex-align-center">
-      正在跳转中...
-    </div>
-  </div>
-</template>
 <script>
 export default {
-  name: "redirect",
-  beforeMount() {
-    console.log(this.$route.query.redirectUrl);
-    console.log(this.$route.query.code);
-    location.href = `${this.$route.query.redirectUrl}?code=${
-      this.$route.query.code
-    }`;
-  }
+  layout: "redirect"
 };
 </script>
-<style>
-.not-exist {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
-
