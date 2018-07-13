cf69a6983b4396a43272f53d2e2926541e662284
dengpeng <dp@tiejin.cn>
2018-07-13 14:13:56 +0800

    Fix test add

--- middleware/user-agent.js
+++ middleware/user-agent.js
@@ -17,6 +17,7 @@ export default async function (context) {
     if (!context.store.state.current_url) {
       context.store.commit('SET_CURRENT_URL', context.req.headers.referer)
     }
+    console.log('context.req.headers.referer====', context.req.headers.referer)
     context.store.commit('GET_AGENT', context.userAgent)
     // 获取并设置当前TOKEN
     context.store.commit('GET_APP_TOKEN', context.req.headers['authorization'])
--- plugins/axios.js
+++ plugins/axios.js
@@ -45,7 +45,6 @@ export default function ({
     }
     // 全局设置 启用跨域传cookies
     config.withCredentials = true
-    // console.log('cooccococ====', store.state.GET_APP_NAV)
     if (store.state.GET_APP_NAV) {
       config.headers.common['X-Udid'] = store.state.h5Cookies || 'h5-SRjHazKvYslHxwZxjy5tql9G4edf3d';
       if (store.state.h5Cookies) {
