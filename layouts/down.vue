d862f85fc5f49197b9b9fd36e997433b59b92eb7
dengpeng <dp@tiejin.cn>
2018-07-28 10:58:39 +0800

    Fix

--- layouts/down.vue
+++ layouts/down.vue
@@ -58,7 +58,7 @@ export default {
   methods: {
     initLoad() {
       let self = this;
-      if (!self.isWeiXin()) {
+      if (self.isWeiXin()) {
         document.querySelector(".downtitle").style.display = "block";
         document.querySelector(".point").style.display = "block";
       } else {
@@ -114,32 +114,6 @@ export default {
       iswb = /weibo/i.test(ua);
       return iswx || isqq || iswb;
     }
-    // getParam(paramName) {
-    //   var paramValue = "";
-    //   var isFound = false;
-    //   if (
-    //     location.search.indexOf("?") == 0 &&
-    //     location.search.indexOf("=") > 1
-    //   ) {
-    //     var arrSource = unescape(location.search)
-    //       .substring(1, location.search.length)
-    //       .split("&");
-    //     var i = 0;
-    //     while (i < arrSource.length && !isFound) {
-    //       if (arrSource[i].indexOf("=") > 0) {
-    //         if (
-    //           arrSource[i].split("=")[0].toLowerCase() ==
-    //           paramName.toLowerCase()
-    //         ) {
-    //           paramValue = arrSource[i].split("=")[1];
-    //           isFound = true;
-    //         }
-    //       }
-    //       i++;
-    //     }
-    //   }
-    //   return paramValue;
-    // }
   },
   beforeMount() {
     let self = this;
