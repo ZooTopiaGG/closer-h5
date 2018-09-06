131c2c31a7426075976099b4f1cea3b68a02b670
邓鹏 <1109142362@qq.com>
2018-09-06 12:04:01 +0800

    fix 留言

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,9 +135,9 @@ module.exports = {
     //   src: '~/plugins/video.js',
     //   ssr: false
     // },
-    {
-      src: '~/plugins/vconsole.js',
-      ssr: false
-    }
+    // {
+    //   src: '~/plugins/vconsole.js',
+    //   ssr: false
+    // }
   ]
 }
