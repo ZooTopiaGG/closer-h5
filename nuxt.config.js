6b56b7a5592cb9a72c92e85362d32fea307899cc
邓鹏 <1109142362@qq.com>
2018-10-31 16:35:12 +0800

    intest 分享

--- nuxt.config.js
+++ nuxt.config.js
@@ -143,9 +143,9 @@ module.exports = {
       src: '~/plugins/video.js',
       ssr: false
     },
-    // {
-    //   src: '~/plugins/vconsole.js',
-    //   ssr: false
-    // }
+    {
+      src: '~/plugins/vconsole.js',
+      ssr: false
+    }
   ]
 }
