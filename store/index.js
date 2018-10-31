7769f99dce9d12334a9495c198c15ab893b8d4d1
邓鹏 <1109142362@qq.com>
2018-10-31 16:41:46 +0800

    rebuild

--- store/index.js
+++ store/index.js
@@ -636,9 +636,10 @@ export const actions = {
       ujson['udid'] = state.h5Cookies;
       ujson['sto'] = 'weixin2share';
       let str = await Coms.args2Url(ujson);
-      url = `${location.protocol}//${location.host}${location.pathname}?${str}`
+      let _url = `${location.protocol}//${location.host}${location.pathname}?${str}`;
+      console.log('url===', _url)
       let para = {
-        url
+        url: _url
       }
       let res = await self.$axios.$post(`${api.share.wechat_config}`, para)
       if (res.code === 0) {
